00e95b45293025084667d556e968102916c42acc
ea887 <ea887@mail.ru>
2026-08-14 22:01:41 +0300

    add: internal page

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@ const logoLight = document.querySelector(".logo-light");
 const logo = document.querySelector(".logo");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
+const isFront = document.body.classList.contains("front-page");
 
 const lightModeOn = (event) => {
   navbar.classList.add("navbar-light");
-  logo.style.display = "block";
-  logoLight.style.display = "none";
-}
+};
 
 const lightModeOff = (event) => {
   navbar.classList.remove("navbar-light");
-  logo.style.display = "none";
-  logoLight.style.display = "block";
-}
+};
+const changeNavHeight = (height) => {
+  navbar.style.height = height;
+};
 
 const openMenu = (event) => { //Функция открывания меню
   menu.classList.add("is-open"); //Вешает класс is-open
@@ -29,7 +29,10 @@ const closeMenu = (event) => { //Функция закрывания меню
   lightModeOff();
 }
 window.addEventListener('scroll', () => {
-  this.scrollY > 1 ? lightModeOn() : lightModeOff();
+  this.scrollY > 1 ? changeNavHeight("4.5rem") : changeNavHeight("5.875rem");
+  if (isFront) {
+    this.scrollY > 1 ? lightModeOn() : lightModeOff();
+  }
 });
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
@@ -88,11 +91,26 @@ const stepsSlider = new Swiper('.steps-slider-wrapper .steps-slider', {
 
 const swiperBlog = new Swiper(".blog-slider", {
   speed: 400,
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 30,
+  allowTouchMove: true,
   navigation: {
     nextEl: '.blog-button-next',
     prevEl: '.blog-button-prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    }
   },
 });
 
