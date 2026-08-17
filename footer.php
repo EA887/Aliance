617c8afcd11193ff77f72876af9deea9e25b061a
ea887 <ea887@mail.ru>
2026-08-17 15:25:24 +0300

    add: bot

--- footer.php
+++ footer.php
@@ -10,12 +10,12 @@
           </p>
           <div class="input-group-wrapper">
             <div class="input-group">
-              <input id="user-name" type="text" class="input" name="username" placeholder=" " />
+              <input id="user-name" type="text" class="input" name="username" placeholder=" " maxlength="100" required />
               <label class="input-group-label" for="user-name">Имя</label>
             </div>
             <!-- /.input-group -->
             <div class="input-group">
-              <input id="user-phone" type="tel" name="userphone" class="input" placeholder=" " />
+              <input id="user-phone" type="tel" name="userphone" class="input" placeholder=" " maxlength="30" required />
               <label class="input-group-label" for="user-phone">Номер телефона</label>
             </div>
             <!-- /.input-group -->
