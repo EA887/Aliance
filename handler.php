959f1d4fae94e47f0985f5eb0baaaa06557f9cbf
ea887 <ea887@mail.ru>
2026-08-24 21:47:39 +0300

    add: phone

--- handler.php
+++ handler.php
@@ -1,7 +1,7 @@
 <?php
 $user_name = htmlspecialchars($_POST["username"]);
 $user_phone = htmlspecialchars($_POST["userphone"]);
-$token = "8289942381:AAElelPE2rdUyPpWCrtPpMeTkyFOfQhPVl8";
+$token = "8289942381:AAEenPfgbakBR-DM-5wzOO7PVQDLMry7ZNk";
 $chat_id = "-5525062708";
 $text = "Клиент: <b>" . htmlspecialchars($user_name, ENT_HTML5, 'UTF-8') . "</b>\n" . "Телефон: <b>" . htmlspecialchars($user_phone, ENT_HTML5, 'UTF-8') . "</b>";
 
@@ -9,7 +9,7 @@ $formData = array(
   "Клиент: " => $user_name,
   "Телефон: " => $user_phone
 );
-foreach($formData as $key => $value){
+foreach($formData as $key => $value) {
   $text .= $key . "<b>" . urlencode($value) . "</b>" . "%0A";
 }
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
@@ -18,4 +18,7 @@ if ($sendToTelegram) {
 } else {
   echo "Error";
 }
+
+
+
 
